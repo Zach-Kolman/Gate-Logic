fe9d843e3053e524735fcf5303a80cf8a26b419c
Zach-Kolman <zach.kolman@gmail.com>
2017-06-14 13:00:28 -0500

    Added basic test level

---
+++ Assets/PlayerCOntroller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCOntroller : MonoBehaviour
+{
+
+	public float maxSpeed = 10f;
+
+	private Vector3 rigi;
+
+	private bool facingRight = true;
+
+	Animator anim;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+		rigi = transform.localScale;
+	}
+
+	void Update ()
+	{
+
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		if (Input.GetAxis ("Horizontal") != 0 && Input.GetAxis ("Vertical") == 0) {
+			anim.SetFloat ("Speed", Mathf.Abs (moveH));
+
+		} else if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") != 0) {
+			anim.SetFloat ("Speed", Mathf.Abs (moveV));
+		}
+
+		if (Input.GetAxis ("Horizontal") != 0 && Input.GetAxis ("Vertical") != 0) {
+			anim.SetFloat ("Speed", Mathf.Abs (moveH));
+		}
+
+		else if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0) {
+			anim.SetFloat ("Speed", Mathf.Abs (moveV));
+		}
+
+	
+
+		if (moveH > 0 && !facingRight)
+			Flip ();
+		else if (moveH < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
