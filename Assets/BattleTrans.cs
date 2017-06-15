085e8b6780624a704c72d5dfd03b37e20286056f
Kolman <ZKOLMAN@depaul.edu>
2017-06-15 17:19:27 -0500

    Added Battle Scene Transition with Enemies

---
+++ Assets/BattleTrans.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleTrans : MonoBehaviour {
+
+	public string target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("This just happened");
+		if (other.gameObject.tag == target) {
+			SceneManager.LoadScene ("BattleTest");
+		}
+	}
+}
