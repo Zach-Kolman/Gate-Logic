9f253d430ed853227d6b78da6cc5170b204ebb2f
Kolman <ZKOLMAN@depaul.edu>
2017-06-21 17:32:24 -0500

    Added prelim button in battle

---
+++ Assets/BattleFlow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleFlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void attackOptions(){
+		Instantiate(GameObject.Find("AttackButton"));
+	}
+}
+
+//First click button
+//then show options for that choice
+//when button is clicked, attack sequence is activated
+//begin enemy attack when player finishes
+//check all players health
+//continue to next round
