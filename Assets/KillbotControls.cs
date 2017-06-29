43218f657cb17aeb5b85a68b8060e9d12945dba9
Your Name <you@example.com>
2017-06-29 17:22:58 -0500

    Added boss health

---
+++ Assets/KillbotControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillbotControls : MonoBehaviour {
+
+	public Text botHP;
+	public bool isHit;
+	public int botHealth = 5;
+	public int curHealth;
+	// Use this for initialization
+	void Start () {
+
+		curHealth = botHealth;
+		isHit = false;
+		botHP.text = curHealth.ToString ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isHit == true) 
+		{
+			curHealth -= 1;
+			isHit = false; 
+		}
+
+		setBotHP ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Debug.Log ("Nothing to see here");
+			isHit = true;
+		}
+	}
+
+	void setBotHP()
+	{
+		botHP.text = curHealth.ToString ();
+	}
+}
