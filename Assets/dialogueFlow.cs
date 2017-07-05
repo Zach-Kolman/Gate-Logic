69d7001e9e4afad62f76aad0ec8947c8850b5f1f
Kolman <ZKOLMAN@depaul.edu>
2017-07-05 16:39:52 -0500

    Revamped assets and started conversatio n system

---
+++ Assets/dialogueFlow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueFlow : MonoBehaviour {
+
+	public GameObject flowchart;
+	public GameObject player;
+	public bool isTalking;
+	// Use this for initialization
+	void Start () {
+		isTalking = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isTalking == true) {
+			player.gameObject.transform.GetChild (2).gameObject.SetActive (true);
+			if (Input.GetKeyDown (KeyCode.E)) {
+				flowchart.SetActive (true);
+			}
+		} else {
+			player.gameObject.transform.GetChild (2).gameObject.SetActive (false);
+		}
+			
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+
+			isTalking = true;
+		}
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+
+			isTalking = false;
+			flowchart.SetActive (false);
+		}
+	}
+}
