d3f7fa656ba9fcc7bb3919985eeca9702b54067c
Kolman <ZKOLMAN@depaul.edu>
2017-08-31 17:23:16 -0500

    Started new Battle system

---
+++ Assets/Scripts/Battle Mechanics/EnemyBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyBase 
+{
+
+	public string name;
+
+	public enum Type
+	{
+		ORGANIC,
+
+		SYNTHETIC,
+
+		POSSESSED
+	}
+
+	public enum Rarity
+	{
+		COMMON,
+
+		UNCOMMON,
+
+		RARE,
+
+		SUPERRARE
+	}
+
+	public Type EnemyType;
+	public Rarity rarity;
+
+	public float baseHP;
+	public float curHP;
+
+	public float baseMP;
+	public float curMP;
+
+	public float baseATK;
+	public float curATK;
+
+	public float baseDEF;
+	public float curDEF;
+
+}
